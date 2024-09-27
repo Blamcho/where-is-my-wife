fe678536438e81fdb08f1a4553dffa4aaf78da3a
Shaasho <zap580@amerike.edu.mx>
2024-09-27 14:44:06 -0600

    Cleared unnecesary methods for Hook State

--- Where_Is_My_Wife/Assets/_Main/Scripts/Player/States/ConcreteStates/PlayerHookState.cs
+++ Where_Is_My_Wife/Assets/_Main/Scripts/Player/States/ConcreteStates/PlayerHookState.cs
@@ -8,16 +8,12 @@ namespace WhereIsMyWife.Player.State
     public class PlayerHookState : PlayerState, IHookState, IHookStateEvents
     {
         public PlayerHookState() : base(PlayerStateMachine.PlayerState.Hook) { }
-        public Action<float> GravityScale { get; set; }
-        public Action<float> FallSpeedCap { get; set; }
         public Action StartHook { get; set; }
         public Action<Vector2> ExecuteHook { get; set; }
-        public Action<Vector2> HookQTEFailed { get; set; }
+        public Action<Vector2> HookQTEEnd { get; set; }
 
         protected override void SubscribeToObservables()
         {
-            _playerStateInput.GravityScale += InvokeGravityScale;
-            _playerStateInput.FallSpeedCap += InvokeFallSpeedCap;
             _playerStateInput.HookActivated += InvokeHookActivated;
             _playerStateInput.ExecuteHookLaunch += InvokeExecuteHook;
             _playerStateInput.HookEnd += InvokeHookEnd;
@@ -25,23 +21,9 @@ namespace WhereIsMyWife.Player.State
 
         protected override void UnsubscribeToObservables()
         {
-            _playerStateInput.GravityScale -= InvokeGravityScale;
-            _playerStateInput.FallSpeedCap -= InvokeFallSpeedCap;
             _playerStateInput.HookActivated -= InvokeHookActivated;
             _playerStateInput.ExecuteHookLaunch -= InvokeExecuteHook;
             _playerStateInput.HookEnd -= InvokeHookEnd;
-        }
-
-        private void InvokeGravityScale(float gravityScale)
-        {
-            NextState = PlayerStateMachine.PlayerState.Movement;
-            GravityScale?.Invoke(gravityScale);
-        }
-
-        private void InvokeFallSpeedCap(float fallSpeedCap)
-        {
-            NextState = PlayerStateMachine.PlayerState.Movement;
-            FallSpeedCap?.Invoke(fallSpeedCap);
         }
 
         private void InvokeHookActivated()
@@ -56,7 +38,8 @@ namespace WhereIsMyWife.Player.State
 
         private void InvokeHookEnd(Vector2 originalVelocity)
         {
-            HookQTEFailed?.Invoke(originalVelocity);
+            HookQTEEnd?.Invoke(originalVelocity);
+            NextState = PlayerStateMachine.PlayerState.Movement;
         }
     }
 }
