a0fed3d8377e845ea08bb96a4610cd675ed1b956
Shaasho <zap580@amerike.edu.mx>
2024-09-28 16:08:55 -0600

    Added Timer to Hook Properties for controlling Hook Behavior

--- Where_Is_My_Wife/Assets/_Main/Scripts/Player/Properties/Modules/PlayerHookProperties.cs
+++ Where_Is_My_Wife/Assets/_Main/Scripts/Player/Properties/Modules/PlayerHookProperties.cs
@@ -5,6 +5,7 @@ namespace WhereIsMyWife.Managers.Properties
     public interface IPlayerHookProperties
     {
         public float ThrustForce { get; }
+        public float TimeToReachHookPosition { get; }
     }
 
     [CreateAssetMenu(menuName = "ScriptableObjects/PlayerProperties/Modules/HookProperties", fileName = "HookProperties")]
@@ -13,6 +14,7 @@ namespace WhereIsMyWife.Managers.Properties
         public IPlayerHookProperties HookProperties => this;
 
         [field: SerializeField] public float ThrustForce { get; private set;}
+        [field: SerializeField] public float TimeToReachHookPosition { get; private set; }
     }
 
 }
