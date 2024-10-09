6d04a978bfadd942cf06ec07c9691fba5b4a50bb
Blamcho <94477654+Blamcho@users.noreply.github.com>
2024-10-08 22:44:51 -0600

    Implementation of lava pipes with horizontal projectiles
    Implementation of lava pipes with horizontal projectiles cannons were implemented that shoot lava balls horizontally from left and right in the part of the level where the lava rises

---
+++ Where_Is_My_Wife/Assets/_Main/Scripts/Stage/LavaProyectil.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class LavaProyectil : MonoBehaviour
+{
+    public float speed = 5f;          
+    public float tiempoDeVida = 8f;   
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+       
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, tiempoDeVida);
+    }
+}
