5173607b5807b606e6ecf62c44af968d84704b65
Ferchus <ferchusgames@protonmail.com>
2024-10-09 13:58:03 -0600

    Fixed the InitialScene not being unloaded
    This was achieved my making the loading LoadSceneMode.Single

--- Where_Is_My_Wife/Assets/_Main/Scripts/Game/LevelManager.cs
+++ Where_Is_My_Wife/Assets/_Main/Scripts/Game/LevelManager.cs
@@ -8,7 +8,7 @@ namespace WhereIsMyWife.Managers
     public class LevelManager : Singleton<LevelManager>
     { 
         [SerializeField] private CanvasGroup _loadingBackground;
-        [SerializeField] private string _lastSceneLoaded;
+        
         public void LoadScene(string sceneName)
         {
             LoadSceneAsync(sceneName).Forget();
@@ -20,24 +20,12 @@ namespace WhereIsMyWife.Managers
             _loadingBackground.gameObject.SetActive(true);
             await _loadingBackground.DOFade(1f, 0.5f).AsyncWaitForCompletion();
             
-            if (!string.IsNullOrEmpty(_lastSceneLoaded))
-            {
-                var unloadOperation = SceneManager.UnloadSceneAsync(_lastSceneLoaded);
-                if (unloadOperation != null)
-                {
-                    await unloadOperation.ToUniTask();
-                }
-            }
-            
-            var asyncOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            var asyncOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
             await asyncOperation.ToUniTask();
-            _lastSceneLoaded = sceneName;
-            
+
             await _loadingBackground.DOFade(0f, 0.5f).AsyncWaitForCompletion();
             _loadingBackground.gameObject.SetActive(false);
         }
-        
-        
     }
 }
 
