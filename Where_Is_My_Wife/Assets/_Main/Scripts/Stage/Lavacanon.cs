4e1ac369a4691f727f575cc542d887e0f72ec87c
Blamcho <94477654+Blamcho@users.noreply.github.com>
2024-10-15 13:51:50 -0600

    Lavacanon Tab script fix in class

--- Where_Is_My_Wife/Assets/_Main/Scripts/Stage/Lavacanon.cs
+++ Where_Is_My_Wife/Assets/_Main/Scripts/Stage/Lavacanon.cs
@@ -2,36 +2,36 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace WhereIsMyWife.Lavacanon
-{
+{ 
     public class LavaCannon : MonoBehaviour
-{
-    [SerializeField] private GameObject _lavaProyectilPrefab; 
-    [SerializeField] private Transform _firePoint;            
-    [SerializeField] private float _minFireRate = 2f;         
-    [SerializeField] private float _maxFireRate = 5f;         
-    private float _nextFireTime = 0f;
-    public bool _shootLeft = true;  
-
-    void Start()
     {
-        _nextFireTime = Time.time + Random.Range(_minFireRate, _maxFireRate);
-    }
+        [SerializeField] private GameObject _lavaProyectilPrefab; 
+        [SerializeField] private Transform _firePoint;            
+        [SerializeField] private float _minFireRate = 2f;         
+        [SerializeField] private float _maxFireRate = 5f;         
+        private float _nextFireTime = 0f;
+        public bool _shootLeft = true;  
 
-    void Update()
-    {
-        if (Time.time >= _nextFireTime)
+        void Start()
         {
-            Shot();
             _nextFireTime = Time.time + Random.Range(_minFireRate, _maxFireRate);
         }
-    }
 
-    void Shot()
-    {
-        GameObject projectile = Instantiate(_lavaProyectilPrefab, _firePoint.position, _firePoint.rotation);
-        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-        float direction = _shootLeft ? -1f : 1f;
-        rb.velocity = _firePoint.right * direction * 5f; 
+        void Update()
+        {
+            if (Time.time >= _nextFireTime)
+            {
+                Shot();
+                _nextFireTime = Time.time + Random.Range(_minFireRate, _maxFireRate);
+            }
+        }
+
+        void Shot()
+        {
+            GameObject projectile = Instantiate(_lavaProyectilPrefab, _firePoint.position, _firePoint.rotation);
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            float direction = _shootLeft ? -1f : 1f;
+            rb.velocity = _firePoint.right * direction * 5f;
+        }
     }
-}
 }
