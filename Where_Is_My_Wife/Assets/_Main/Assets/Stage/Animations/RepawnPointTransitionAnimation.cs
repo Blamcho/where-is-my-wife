e98ab2e1c39ea6f9c7a7c52565f71e46011eb674
Blamcho <94477654+Blamcho@users.noreply.github.com>
2024-10-23 13:46:37 -0600

    add namespace and remove obsolete animation objects

--- Where_Is_My_Wife/Assets/_Main/Assets/Stage/Animations/RepawnPointTransitionAnimation.cs
+++ Where_Is_My_Wife/Assets/_Main/Assets/Stage/Animations/RepawnPointTransitionAnimation.cs
@@ -1,54 +1,54 @@
 using UnityEngine;
 using WhereIsMyWife.Managers;
-using UnityEngine.UI;
 using WhereIsMyWife.Controllers;
 
-public class RespawPointTrasition : MonoBehaviour
+namespace WhereIsMyWife.Controllers
 {
-    IRespawn _playerRespawn;
-   
-    [SerializeField] private Transform _playerRespawnTransform;
-    [SerializeField] private GameObject _respawnPanel;  
-     public Animator _respawnAnimator;
-     
-    private void Start()
-    {
-        if (PlayerManager.Instance != null && PlayerManager.Instance.Respawn != null)
+        public class RespawPointTrasition : MonoBehaviour
         {
-            _playerRespawn = PlayerManager.Instance.Respawn;
-        }
-        else
-        {
-            Debug.LogError("PlayerManager o Respawn es null");
-        }
+            IRespawn _playerRespawn;
 
-        if (_respawnAnimator == null)
-        {
-            Debug.LogError("Animator no asignado en el Inspector");
+            [SerializeField] private Transform _playerRespawnTransform;
+            [SerializeField] private GameObject _respawnPanel;
+            public Animator _respawnAnimator;
+
+            private void Start()
+            {
+                if (PlayerManager.Instance != null && PlayerManager.Instance.Respawn != null)
+                {
+                    _playerRespawn = PlayerManager.Instance.Respawn;
+                }
+                else
+                {
+                    Debug.LogError("PlayerManager o Respawn es null");
+                }
+
+                if (_respawnAnimator == null)
+                {
+                    Debug.LogError("Animator no asignado en el Inspector");
+                }
+            }
+
+            private void OnTriggerEnter2D(Collider2D other)
+            {
+                if (other.CompareTag("StageCollider"))
+                {
+
+                    AnimateRespawnTransition(() => { _playerRespawn.SetRespawnPoint(_playerRespawnTransform.position); });
+                }
+            }
+
+            private void AnimateRespawnTransition(System.Action onTransitionComplete)
+            {
+                _respawnAnimator.SetTrigger("StartTransition");
+
+                Invoke(nameof(RespawnComplete), 1f);
+                onTransitionComplete?.Invoke();
+            }
+
+            private void RespawnComplete()
+            {
+                Debug.Log("Respawn completado.");
+            }
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("StageCollider"))
-        {
-         
-            AnimateRespawnTransition(() => {
-                _playerRespawn.SetRespawnPoint(_playerRespawnTransform.position);
-            });
-        }
-    }
-
-    private void AnimateRespawnTransition(System.Action onTransitionComplete)
-    {
-        _respawnAnimator.SetTrigger("StartTransition");
-        
-        Invoke(nameof(RespawnComplete), 1f); 
-        onTransitionComplete?.Invoke();
-    }
-
-    private void RespawnComplete()
-    {
-        Debug.Log("Respawn completado.");
-    }
-}
--- Where_Is_My_Wife/Assets/_Main/Assets/Stage/Animations/RespawnCurtainsAnimation.cs
+++ Where_Is_My_Wife/Assets/_Main/Assets/Stage/Animations/RespawnCurtainsAnimation.cs
@@ -5,51 +5,53 @@ using UnityEngine;
 using UnityEngine.UI;
 using WhereIsMyWife.Managers;
 
-
-public class RespawnCurtainsAnimation : MonoBehaviour
+namespace WhereIsMyWife.Controllers
 {
-    [SerializeField] private Image _leftPanel;
-    [SerializeField] private Image _rightPanel;
-    [SerializeField] private float _animationDuration;
-    [SerializeField] private float _closedTime;
-    [SerializeField] private Ease _ease = Ease.InSine;
+        public class RespawnCurtainsAnimation : MonoBehaviour
+        {
+            [SerializeField] private Image _leftPanel;
+            [SerializeField] private Image _rightPanel;
+            [SerializeField] private float _animationDuration;
+            [SerializeField] private float _closedTime;
+            [SerializeField] private Ease _ease = Ease.InSine;
 
-    private float _panelWidth;
+            private float _panelWidth;
 
-    private void Awake()
-    {
-        _panelWidth = _leftPanel.rectTransform.rect.width;
+            private void Awake()
+            {
+                _panelWidth = _leftPanel.rectTransform.rect.width;
+            }
+
+            private void Start()
+            {
+                PlayerManager.Instance.DeathAction += StartAnimation;
+            }
+
+            private void OnDestroy()
+            {
+                PlayerManager.Instance.DeathAction -= StartAnimation;
+            }
+
+            private void StartAnimation()
+            {
+                AnimationAsync().Forget();
+            }
+
+            private async UniTaskVoid AnimationAsync()
+            {
+                _leftPanel.rectTransform.DOAnchorPosX(_panelWidth, _animationDuration).SetEase(_ease);
+                await _rightPanel.rectTransform.DOAnchorPosX(-_panelWidth, _animationDuration)
+                    .SetEase(_ease).AsyncWaitForCompletion();
+
+                await UniTask.Delay(TimeSpan.FromSeconds(_closedTime));
+
+                PlayerManager.Instance.TriggerRespawnStart();
+
+                _leftPanel.rectTransform.DOAnchorPosX(0, _animationDuration).SetEase(_ease);
+                await _rightPanel.rectTransform.DOAnchorPosX(0, _animationDuration)
+                    .SetEase(_ease).AsyncWaitForCompletion();
+
+                PlayerManager.Instance.TriggerRespawnComplete();
+            }
+        }
     }
-
-    private void Start()
-    {
-        PlayerManager.Instance.DeathAction += StartAnimation;
-    }
-
-    private void OnDestroy()
-    {
-        PlayerManager.Instance.DeathAction -= StartAnimation;
-    }
-
-    private void StartAnimation()
-    {
-        AnimationAsync().Forget();
-    }
-    
-    private async UniTaskVoid AnimationAsync()
-    {
-        _leftPanel.rectTransform.DOAnchorPosX(_panelWidth, _animationDuration).SetEase(_ease);
-        await _rightPanel.rectTransform.DOAnchorPosX(-_panelWidth, _animationDuration)
-            .SetEase(_ease).AsyncWaitForCompletion();
-
-        await UniTask.Delay(TimeSpan.FromSeconds(_closedTime));
-        
-        PlayerManager.Instance.TriggerRespawnStart();
-        
-        _leftPanel.rectTransform.DOAnchorPosX(0, _animationDuration).SetEase(_ease);
-        await _rightPanel.rectTransform.DOAnchorPosX(0, _animationDuration)
-            .SetEase(_ease).AsyncWaitForCompletion();
-        
-        PlayerManager.Instance.TriggerRespawnComplete();
-    }
-}
