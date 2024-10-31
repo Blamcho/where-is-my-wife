7fdc0a23a6ad89965fe1f90c03e10825f803d3fb
Abraham Torres <94477654+Blamcho@users.noreply.github.com>
2024-10-31 11:57:05 -0600

    Level Change Animations (#40)
    * Implementation of Storyboard Animations
    
    * Next Level Button Settings and Final Level Settings

---
+++ Where_Is_My_Wife/Assets/_Main/Scenes/LevelChangeaAnimations/LevelStoryBoardAnimation.cs
@@ -0,0 +1,40 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+namespace WhereIsMyWife.SceneManagement
+{
+    public class LevelStoryBoardAnimation : MonoBehaviour
+    {
+        [SerializeField] private Button _nextSceneButton;
+        [SerializeField] private Image[] comicImages; 
+        [SerializeField] private float fadeDuration = 1f; 
+        [SerializeField] private float intervalBetweenImages = 2f;
+
+        private void Start()
+        {
+            StoryBoardAnimationAsync().Forget();
+        }
+
+        private async UniTaskVoid StoryBoardAnimationAsync()
+        {
+            _nextSceneButton.interactable = false;
+            _nextSceneButton.gameObject.SetActive(false);
+            foreach (var image in comicImages)
+            {
+                image.color = new Color(1, 1, 1, 0);
+            }
+            
+            for (int i = 0; i < comicImages.Length; i++)
+            {
+                comicImages[i].DOFade(1, fadeDuration); 
+                await UniTask.Delay((int)((fadeDuration + intervalBetweenImages) * 1000));
+            }
+            _nextSceneButton.gameObject.SetActive(true);
+            _nextSceneButton.interactable = true;
+            _nextSceneButton.Select();
+        }
+    }
+}
