768e3df3caa95d4f8664d25d8708497f8f4afef1
Shasho <zap580@amerike.edu.mx>
2024-11-06 18:55:59 -0600

    Removed unused libraries in hook script and changed player position

--- Where_Is_My_Wife/Assets/Hook_QTE/Scripts/HookPoint.cs
+++ Where_Is_My_Wife/Assets/Hook_QTE/Scripts/HookPoint.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Overlays;
 using UnityEngine;
 using WhereIsMyWife.Controllers;
 using WhereIsMyWife.Managers;
