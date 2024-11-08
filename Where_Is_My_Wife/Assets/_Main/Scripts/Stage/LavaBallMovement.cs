739919aad3646f9b7a3cad4b83f4b8d35048b156
Blamcho <94477654+Blamcho@users.noreply.github.com>
2024-11-08 09:54:21 -0600

    lava ball sprite spin mods

--- Where_Is_My_Wife/Assets/_Main/Scripts/Stage/LavaBallMovement.cs
+++ Where_Is_My_Wife/Assets/_Main/Scripts/Stage/LavaBallMovement.cs
@@ -1,23 +1,38 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 
-namespace WhereIsMyWife.lava_ballvertical_motion
+namespace WhereIsMyWife.LavaBalls
 {
     public class LavaBallMovement : MonoBehaviour
-    { 
+    {
         [SerializeField] private float _speed = 2f;
         [SerializeField] private float _height = 3f;
         private Vector3 _startPos;
+        private SpriteRenderer _spriteRenderer;
+        private float _lastY;
 
         void Start()
         {
             _startPos = transform.position;
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _lastY = _startPos.y;
         }
 
         void Update()
         {
             float newY = Mathf.PingPong(Time.time * _speed, _height);
             transform.position = new Vector3(_startPos.x, _startPos.y + newY, _startPos.z);
+
+            if (transform.position.y < _lastY)
+            {
+                _spriteRenderer.flipY = true;
+            }
+            else if (transform.position.y > _lastY)
+            {
+                _spriteRenderer.flipY = false;
+            }
+
+            _lastY = transform.position.y;
         }
     }
 }
