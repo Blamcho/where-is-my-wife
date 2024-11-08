058e3a1281b54d9583faac7c2bbaae68f775cbc0
Ivan Herrera <ivan.herrera.m@outlook.com>
2024-11-08 13:39:45 -0600

    Fixed Story prefab and scripts

--- Where_Is_My_Wife/Assets/_Main/Scripts/Story/StoryBoardAnimation.cs
+++ Where_Is_My_Wife/Assets/_Main/Scripts/Story/StoryBoardAnimation.cs
@@ -1,15 +1,17 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
-using WhereIsMyWife.Managers;
+using DG.Tweening;
 
 namespace WhereIsMyWife.SceneManagement
 {
     public class StoryBoardAnimation : MonoBehaviour
     {
         [SerializeField] private Button _nextSceneButton;
-        
+        [SerializeField] private Image[] comicImages; 
+        [SerializeField] private float fadeDuration = 1f; 
+        [SerializeField] private float intervalBetweenImages = 2f;
+
         private void Start()
         {
             StoryBoardAnimationAsync().Forget();
@@ -18,9 +20,18 @@ namespace WhereIsMyWife.SceneManagement
         private async UniTaskVoid StoryBoardAnimationAsync()
         {
             _nextSceneButton.interactable = false;
+            _nextSceneButton.gameObject.SetActive(false);
+            foreach (var image in comicImages)
+            {
+                image.color = new Color(1, 1, 1, 0);
+            }
             
-            // TODO: Add story board animation 
-            
+            for (int i = 0; i < comicImages.Length; i++)
+            {
+                comicImages[i].DOFade(1, fadeDuration); 
+                await UniTask.Delay((int)((fadeDuration + intervalBetweenImages) * 1000));
+            }
+            _nextSceneButton.gameObject.SetActive(true);
             _nextSceneButton.interactable = true;
             _nextSceneButton.Select();
         }
