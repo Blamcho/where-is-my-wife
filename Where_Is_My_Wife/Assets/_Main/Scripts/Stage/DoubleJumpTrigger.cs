e8a61a5f266b5beb75e6350958bc05928b99c8e3
Shasho <zap580@amerike.edu.mx>
2024-11-25 13:57:47 -0600

    Fixes after rebase

--- Where_Is_My_Wife/Assets/_Main/Scripts/Stage/DoubleJumpTrigger.cs
+++ Where_Is_My_Wife/Assets/_Main/Scripts/Stage/DoubleJumpTrigger.cs
@@ -6,8 +6,6 @@ namespace WhereIsMyWife.Stage
     public class DoubleJumpTrigger : MonoBehaviour
     {
         [SerializeField] private Color _activeColor = Color.green;
-        [SerializeField] private float _respawnDelay = 5f;
-        [SerializeField] private float _respawnFadeDuration = 0.5f;
         
         private SpriteRenderer _spriteRenderer;
         private Tween _respawnTween;
